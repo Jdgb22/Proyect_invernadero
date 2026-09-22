ad97a38b4407c2c2be29835642fed57446d0f857
Juan <jg663599@gmail.com>
2026-09-22 09:39:14 -0500

    feat: allow all hosts in vite server configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -11,6 +11,9 @@ export default defineConfig({
   adapter: vercel(),
   integrations: [auth()],
   vite: {
-    plugins: [tailwindcss()]
+    plugins: [tailwindcss()],
+    server: {
+      allowedHosts: true
+    }
   }
 });
